fed03718c2f0c1e195a79b155f47b1de7041f290
akshitha-mothkur <akshithamothkur2686@gmail.com>
2026-06-27 13:23:57 +0530

    created MyJournals page with My journals and new journal

--- conscious-consumption/src/App.jsx
+++ conscious-consumption/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-
+import MyJournals from './components/MyJournals'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +13,7 @@ function App() {
      <Navbar/>
      <Routes>
       <Route path='/' Component={Home}/>
+      <Route path='/myJournals' Component={MyJournals}/>
      </Routes>
      </BrowserRouter>
     </>
---
+++ conscious-consumption/src/components/MyJournals.jsx
@@ -0,0 +1,14 @@
+import '../styles/MyJournals.css'
+
+function MyJournals(){
+    return(
+        <>
+        <div className="upper">
+            <h2>My Journals</h2>
+            <button id="btn">+ New Journal</button>
+        </div>
+        </>
+    )
+}
+
+export default MyJournals
